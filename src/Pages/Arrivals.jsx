1447b4f7eb0a0fa603856365ae56aebf95d4c07d
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-07-06 19:11:55 +0530

    arrivals done.

--- src/Pages/Arrivals.jsx
+++ src/Pages/Arrivals.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import arrivals from "../Images/arrivals-Photoroom.png";
+import Para from "../Components/Para";
+import Button from "../Components/Button";
 
 const Arrivals = () => {
-  return <div>Arrivals</div>;
+  return (
+    <div className="arrivals">
+      <div className="arrivals-img">
+        <img className="" src={arrivals} alt="arrivals" />
+      </div>
+      <span className="arrivals-title">new arrivals</span>
+      <div className="arrivals-para">
+        <Para />
+      </div>
+      <div className="arrivals-button">
+        <Button />
+      </div>
+    </div>
+  );
 };
 
 export default Arrivals;

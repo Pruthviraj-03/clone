1df1605f7e3a6a26655906ec7ba3465eec6840cb
Pruthviraj-03 <pruthvirajkurane03@gmail.com>
2024-07-06 18:46:31 +0530

    header done.

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-one">
+        <input type="text" placeholder="SEARCH"></input>
+      </div>
+      <div className="header-two">
+        <ul className="header-nav-list">
+          <li>login</li>
+          <li>help</li>
+          <li>shopping bag (0)</li>
+        </ul>
+      </div>
+      <span className="center-span">house of orgin</span>
+    </div>
+  );
+};
+
+export default Header;
